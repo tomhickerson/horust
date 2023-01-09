d46b5e5804619bc778145dacf432231a9910eb5f
Tom Hickerson <tom@thehillsidegroup.com>
2023-01-09 15:22:26 +0200

    update to factorials

--- ch1/src/main.rs
+++ ch1/src/main.rs
@@ -3,6 +3,19 @@ fn main() {
     // Chapter one - Recursion and Callbacks
 
     // Decimal to Binary Conversion 
-
+    let mut st1:String = String::new();
+    st1.push('a');
     // Factorials 
+    let x:u64 = factorial(6);
+    println!("{}", x);
+
+
+
+}
+
+fn factorial(n: u64) -> u64 {
+    match n {
+        0 | 1 => 1,
+        n     => n * factorial(n - 1), 
+    }
 }
