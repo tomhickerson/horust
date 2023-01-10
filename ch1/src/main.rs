ccd427807e6dbe762186be99aae011e384320255
Tom Hickerson <tom@thehillsidegroup.com>
2023-01-10 13:30:43 +0200

    better version of binary

--- ch1/src/main.rs
+++ ch1/src/main.rs
@@ -7,7 +7,10 @@ fn main() {
     st1.push('a');
     // Factorials 
     let x:u64 = factorial(6);
-    println!("{}", x);
+    println!("factorial: {}", x);
+
+    let y:i32 = to_binary(16);
+    println!("to binary: {}", y);
 }
 
 fn factorial(n: u64) -> u64 {
@@ -17,12 +20,26 @@ fn factorial(n: u64) -> u64 {
     }
 }
 
-fn binary(n: u64) -> String {
-    let mut ret:String = String:new();
-    match n {
-        0    => ret.push('0'),
-        1    => ret.push('1'),
-        _    => binary(n % 2),
-    }
+fn to_binary(mut decimal: i32) -> i32 {
+    if decimal == 0 {
+        decimal
+    } else {
+        let mut bits = String::new();
 
+        while decimal > 0 {
+            if decimal % 2 == 0 {
+                bits.push_str("0");
+            } else {
+                bits.push_str("1");
+            }
+
+            decimal /= 2;
+        }
+
+        // reverse the bits
+        match bits.chars().rev().collect::<String>().parse() {
+            Ok(num) => num,
+            Err(_e) => panic!("Something went wrong"),
+        }
+    }
 }
