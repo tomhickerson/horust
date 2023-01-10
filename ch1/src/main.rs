195e82fdf1992c8aac8ac38944bc433c43eba4be
Tom Hickerson <tom@thehillsidegroup.com>
2023-01-10 13:24:42 +0200

    first attempt at binary, not very good tho

--- ch1/src/main.rs
+++ ch1/src/main.rs
@@ -8,9 +8,6 @@ fn main() {
     // Factorials 
     let x:u64 = factorial(6);
     println!("{}", x);
-
-
-
 }
 
 fn factorial(n: u64) -> u64 {
@@ -18,4 +15,14 @@ fn factorial(n: u64) -> u64 {
         0 | 1 => 1,
         n     => n * factorial(n - 1), 
     }
+}
+
+fn binary(n: u64) -> String {
+    let mut ret:String = String:new();
+    match n {
+        0    => ret.push('0'),
+        1    => ret.push('1'),
+        _    => binary(n % 2),
+    }
+
 }
